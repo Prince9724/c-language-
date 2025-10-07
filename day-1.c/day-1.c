e696e30faf40591dedba4d8fdd39a3a367b74ceb
Prince gond <princegondrw123@gmail.com>
2025-10-07 18:44:25 +0530

    Add files via upload

---
+++ day-1.c/day-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("Basic Information\n");
+	printf("---------------------------\n");
+ printf("Hello\n");
+ printf("Name - Prince Gond\n");
+ printf("age - 17\n");
+ printf("contact no. - 9724672317\n");
+ printf( "Course - Full Stack Web Developer\n");
+ printf("Degree - 12th");
+}
