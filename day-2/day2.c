dc56d39e2578a1e31f1b8041012fbc3447f4e8ea
Prince gond <princegondrw123@gmail.com>
2025-10-09 16:01:13 +0530

    Add files via upload

---
+++ day-2/day2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ int t = 203;
+  printf( " 203\n");
+  printf("----------------------\n");
+ 
+	printf("%d * 1 = %d\n",t,t*1);
+	printf("%d * 2 = %d\n",t,t*2);
+	printf("%d * 3 = %d\n",t,t*3);
+	printf("%d * 4 = %d\n",t,t*4);
+	printf("%d * 5 = %d\n",t,t*5);
+	printf("%d * 6 = %d\n",t,t*6);
+	printf("%d * 7 = %d\n",t,t*7);
+	printf("%d * 8 = %d\n",t,t*8);
+	printf("%d * 9 = %d\n",t,t*9);
+	printf("%d * 10 = %d\n",t,t*10);
+	
+	
+	 printf("--x+y=?/n");
+	 printf("----------------------\n");
+
+	
+    int x = 20;
+    int y = 30;
+    	printf("\n");
+		printf("%d + %d = %d",x,y,x+y);	
+ }
+ 
+ 
+ 
+ 	
+ 
+ 
+ 
