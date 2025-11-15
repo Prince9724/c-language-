0b4b64f4f4fd310c63deee853e6d86de722f5075
Prince gond <princegondrw123@gmail.com>
2025-11-15 19:02:08 +0530

    Add files via upload

---
+++ day-6/program.c
@@ -0,0 +1,48 @@
+
+#include<stdio.h>
+void main (){
+//int  i = 1;
+//while(i<=10){
+//	printf("hello word.\n",i);
+//	
+//	i++;
+//}	
+//	int i = ;
+//	while(){
+//		printf("");
+//		 
+//	}
+//		
+
+//	 3. ----> even or odd
+
+
+//int n;
+//printf("Enter n :");
+//scanf("%d",&n);
+//int i=2;
+//
+//
+//  while(i<=n){
+//	if(i%2==1)
+//	{
+//		printf("%d ",i);
+//	}
+//	i++;
+//}
+//	
+// 4-----> leep year 
+int n ;
+printf("Enter number: ");
+scanf("%d",&n);
+while(i<=n){
+	if(n % 4==0){
+		printf("%d ",i);
+	}
+	do{
+		printf("this is not leap year");
+	}
+}
+}
+
+
