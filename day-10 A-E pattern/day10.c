c4d46ce5e2b37b1605edc53da3d601615d4e0e1d
Prince gond <princegondrw123@gmail.com>
2025-11-21 15:46:24 +0530

    Add files via upload

---
+++ day-10 A-E pattern/day10.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+void main(){
+// A
+//A B
+//A B C
+//A B C D
+//A B C D E
+
+//	int i, j;
+//	char alfa;
+//		for(i='A';i<='E';i++)
+//		{
+//		for(j='A';j<=i;j++){
+//			printf("%c ",j);
+//		}
+//			printf("\n");
+//			}	
+//
+
+//	A 
+//	B B
+//	C C C
+//	D D D D
+//	E E E E E
+
+//	int i, j;
+//	char alfa;
+//		for(i='A';i<='E';i++)
+//		{
+//		for(j='A';j<=i;j++){
+//			printf("%c ",i);
+//		}
+//			printf("\n");
+//			}	
+
+//A
+//B C
+//D E F
+//G H I J
+//K L M N O
+
+//	int i, j;
+//	char alfa = 'A';
+//		for(i='A';i<='E';i++)
+//		{
+//		for(j='A';j<=i;j++){
+//			printf("%c ",alfa);
+//			alfa++;
+//		}
+//			printf("\n");
+//			}	
+
+
+//E E E E E
+//D D D D
+//C C C
+//B B
+//A
+
+//	int i, j;
+//	char alfa;
+//		for(i='E';i>='A';i--)
+//		{
+//		for(j='A';j<=i;j++){
+//			printf("%c ",i);
+//		}
+//			printf("\n");
+//			}	
+
+//A B C D E
+//A B C D
+//A B C
+//A B
+//A
+
+//	int i, j;
+//	char alfa;
+//		for(i='E';i>='A';i--)
+//		{
+//		for(j='A';j<=i;j++){
+//			printf("%c ",j);
+//		}
+//			printf("\n");
+//			}	
+
+
+}
