323941a5aacd026aa1726d029fd77f2d79d621a6
Prince gond <princegondrw123@gmail.com>
2025-11-28 17:03:36 +0530

    Add files via upload

---
+++ day-14 array/array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main(){
+//	int a [6]={1,2,3,4,5,6,};
+//	printf("%d", a[4]);
+//	printf ("%d", a[2]); // one time only one value print
+//int a [6]={1,2,3,4,5,6,};
+//	Wap program to find even number in array.
+//	int i;
+//	for(i=1 ;i<6;i++){
+//		
+//		if(a[i]%2==0)
+//		{
+//			printf("%d ",a[i]);
+//		}
+//	}
+	
+//	user input ke liye 
+	int a[6];
+	int i;
+	 for(i=0;i<6;i++)
+	 {
+	 	printf("Enter a[%d]", i);
+	 	scanf("%d",&a[i]);
+	 }
+	l
+	
+ 
+	for(i=1 ;i<6;i++){
+		
+		if(a[i]%2==0)
+		{
+			printf("%d ",a[i]);
+		}
+	}
+}
