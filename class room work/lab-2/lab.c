90f240d0c217a1b0d5424acb5f75a18c984f060c
Prince gond <princegondrw123@gmail.com>
2025-12-01 13:59:44 +0530

    Add files via upload

---
+++ class room work/lab-2/lab.c
@@ -0,0 +1,129 @@
+
+#include<stdio.h>
+void main (){
+	
+// 1.
+//	int i,j,k;
+//	for(i=1;i<=5;i++){
+//	 for(k=i;k<5;k++){
+//			printf("  ");
+//		}
+//		for(j=1;j<=i;j++){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+	
+//	2.
+//	int i,j,k;
+//	for(i=1;i<=5;i++){
+//	 for(k=i;k<5;k++){
+//			printf("  ");
+//		}
+//		for(j=i;j>=1;j--){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+
+//	3.
+
+//	int i,j,k;
+//	for(i=5;i>=1;i--){
+//	  	 for(k=1;k<i;k++){
+//			printf("  ");
+//		}
+//		for(j=i;j<=5;j++){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+
+//	4.
+//	int i,j,k;
+//	for(i=5;i<=5;i++){
+//	  	 for(k=5;k>i;k--){
+//			printf("  ");
+//		}
+//		for(j=5;j<=6-1;j--){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	} // smjh nhi aa rha hai sir ji
+
+//	5.
+//	int i,j,k;
+//	for(i=1;i<=5;i++){
+//	 for(k=i;k<5;k++){
+//			printf("  ");
+//		}
+//		for(j=i;j>=1;j--){
+//			printf("%d ", i);
+//		}
+//		printf("\n");
+//	}
+
+//	6.
+//	int i,j,k;
+//	for(i=5;i>=1;i--){
+//	  	 for(k=1;k<i;k++){
+//			printf("  ");
+//		}
+//		for(j=i;j<=5;j++){
+//			printf("%d ", i);
+//		}
+//		printf("\n");
+//	}
+
+//	7.
+//		int i,j,k;
+//	for(i=5;i>=1;i--){
+//	  	 for(k=5;k>i;k--){
+//			printf("  ");
+//		}
+//		for(j=1;j<=i;j++){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+
+//	8.
+
+//		int i,j,k;
+//	for(i=1;i<=5;i++){
+//	  	 for(k=1;k<i;k++){
+//			printf("  ");
+//		}
+//		for(j=i;j<=5;j++){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+
+//	9.
+
+//		int i,j,k;
+//	for(i=1;i<=5;i++){
+//	  	 for(k=1;k<i;k++){
+//			printf("  ");
+//		}
+//		for(j=5;j>=i;j--){
+//			printf("%d ", j);
+//		}
+//		printf("\n");
+//	}
+
+//	10.
+	int i,j,k;
+	for(i=1;i<=5;i++){
+	  	 for(k=1;k<i;k++){
+			printf("  ");
+		}
+		for(j=i;j<=5;j++){
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
+ 
+
+}
