db70b360611e551c6ec9cde399fd3a70efd595e0
Prince gond <princegondrw123@gmail.com>
2025-12-04 16:56:46 +0530

    Add files via upload

---
+++ day-20/day.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+ char name[20];
+ printf("Enter name : ");
+ gets(name);
+// puts(name);
+//int i = ;
+//printf(" length is : %d ",strlen(name)); // length size 
+
+//puts(strupr(name));// convert to small to capital .
+//printf("%d ",strlwr(name));
+//strcpy(name,"new name");// new value assingn in variable/exchange name.
+//puts(name);
+
+//strrev(name,"hello"); // reverse  string of string :name = eman.
+//printf("%s ", name);
+//strcat(name,"Happy","Prince");
+//printf("%s ",name);---> douts?
+
+
+
+}
