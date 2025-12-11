a80d8761ba80d89bf2b99b140e1139ca62dd47f0
Prince gond <princegondrw123@gmail.com>
2025-12-11 15:15:28 +0530

    Add files via upload

---
+++ prj-7/calculater.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+//Function declartion
+int sum();
+int sub();
+int mul();
+int div();
+int mod();
+
+//function Definition
+int sum(int a,int b)
+{
+    return a + b;
+}
+int sub(int a,int b)
+{
+    return a - b;
+}
+int mul(int a,int b)
+{
+    return a * b;
+}
+int div(int a,int b)
+{
+    return a / b;
+}
+int mod(int a,int b)
+{
+    return a % b;
+}
+
+int main()
+{
+    { 
+	  int choice,result,a,b;//variable declartion
+        printf("Press 1 for +");//Addition
+        printf("\nPress 2 for -");//Subtraction
+        printf("\nPress 3 for *");//Multiplaction
+        printf("\nPress 4 for /");//Division
+        printf("\nPress 5 for %");//module
+                printf("\nPress 5 for %");//module
+
+        printf("\nPress 0 for Exit");// Exit from the Calculator program
+
+
+        printf("\n\nEnter Your Choice : ");
+        scanf("%d",&choice);
+        if (choice==0)
+        {
+            printf("\nExit !\nYou are Exited From The Calculator program !");
+        }
+        else {
+            printf("\nEnter First Number : ");//user input message
+            scanf("%d",&a);//user input
+            printf("\nEnter Second Number : ");//user input message
+            scanf("%d",&b);//user input
+
+            switch(choice)
+            {
+
+            case 1://sum
+                result = sum(a,b); 
+                printf("\nAddition of %d + %d = %d ",a,b,result); 
+                break;
+            case 2 ://subtraction
+                result = sub(a,b); 
+                printf("\nSubtraction of %d - %d = %d ",a,b,result);  
+                break;
+            case 3 : // Multiplication
+                result = mul(a,b);//catch and store return value
+                printf("\nMultication of %d * %d = %d",a,b,result); 
+                break;
+
+            case 4 :  
+                result = div(a,b); 
+                if(b==0)
+                {
+                    printf("\nDenominator should not equal to 0 !");
+                    return 0;
+                }
+                printf("\nDivision of %d / %d = %d",a,b,result); 
+                break;
+            case 5 : // Module
+                result = mod(a,b); 
+                if(b==0)
+                {
+                    printf("\nb = 0 ! Not Allowed ");
+                    return 0;
+                }
+                printf("\nModule = %d",result); 
+                break;
+            default:
+                printf("\n\nInvalid !\nPlease Enter Number between 1 to 5");
+            }
+        }
+    }
+}
