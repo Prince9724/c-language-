afcee19210d1276ea9eede4cf63c8e33cc2e513a
Prince gond <princegondrw123@gmail.com>
2025-12-12 15:20:41 +0530

    Add files via upload

--- prj-6/question-1.c
+++ prj-6/question-1.c
@@ -3,25 +3,24 @@
 int main() {
     char str[100];
     int i, length = 0,Palindrome = 1;//variable declaration
-	int o=15246548 ;
-	printf("%d ",o);
-//    printf("Enter a string: ");//user input message
-//    scanf("%s", str);//user input
-//    while (str[length] != '\0') {
-//        length++;
-//    }
-//    for (i = 0; i < length / 2; i++) {
-//        if (str[i] != str[length - i - 1]) {
-//            Palindrome = 0;
-//            break;
-//        }
-//    }//douts line no.12?
-//
-//    if (Palindrome)
-//        printf("The string is a palindrome.\n");//print if palindrome
-//    else
-//        printf("The string is not a palindrome.\n");//print if not palindrome
+
+    printf("Enter a string: ");//user input message
+    scanf("%s", str);//user input
+    while (str[length] != '\0') {
+        length++;
+    }
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            Palindrome = 0;
+            break;
+        }
+//    }//douts line no.12
+
+    if (Palindrome)
+        printf("The string is a palindrome.\n");//print if palindrome
+    else
+        printf("The string is not a palindrome.\n");//print if not palindrome
 
 }
 // str = ans;
-//for(i = 0;)
+for(i = 0;)
